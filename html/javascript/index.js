9fb94be28a21b8a3d978383c1b076710dc452347
Sanjayeng <sanjay2pu@gmail.com>
2025-11-03 09:40:43 +0530

    adding new files

---
+++ html/javascript/index.js
@@ -0,0 +1,13 @@
+a=3;
+b=2;
+c=4;
+
+if(a<b && a<c){
+    console.log('a is the smallest number');
+}
+else if(b<a && b<c){
+    console.log('b is the smallest number');
+}
+else{
+    console.log('c is the smallest number');
+}
---
+++ html/javascript/while.js
@@ -0,0 +1,4 @@
+function factorial_of_number(){
+    n1=1
+    console.log(n1*n-1)
+}
